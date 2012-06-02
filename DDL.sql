4d2f334a76c7fb6182db5ed036f19569fdeb15b2
trent <trent.schafer@gmail.com>
2012-06-02 13:26:07 +1000

    Add supporting objects file, with first table DDL

---
+++ DDL.sql
@@ -0,0 +1,27 @@
+CREATE table "TS_ERROR_CODE" (
+    "ERROR_CODE_ID" NUMBER NOT NULL,
+    "CODE"          NUMBER NOT NULL,
+    "DESCRIPTION"   VARCHAR2(4000) NOT NULL,
+    "CAUSE"         VARCHAR2(4000) NOT NULL,
+    "ACTION"        VARCHAR2(4000) NOT NULL,
+    constraint  "TS_ERROR_CODE_PK" primary key ("ERROR_CODE_ID")
+)
+/
+
+CREATE sequence "TS_ERROR_CODE_SEQ" 
+/
+
+CREATE trigger "BI_TS_ERROR_CODE"  
+  before insert on "TS_ERROR_CODE"              
+  for each row 
+begin  
+  if :NEW."ERROR_CODE_ID" is null then
+    select "TS_ERROR_CODE_SEQ".nextval into :NEW."ERROR_CODE_ID" from dual;
+  end if;
+end;
+/   
+
+alter table "TS_ERROR_CODE" add
+constraint "TS_ERROR_CODE_UK1" 
+unique ("CODE")
+/   
