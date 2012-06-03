2ff12cb33fc5b13bf3caeb569ba355ad06413b56
trent <trent.schafer@gmail.com>
2012-06-03 15:11:25 +1000

    Change table to have ora code as the primary key

--- DDL.sql
+++ DDL.sql
@@ -1,31 +1,9 @@
-CREATE table "TS_ERROR_CODE" (
-    "ERROR_CODE_ID" NUMBER NOT NULL,
-    "CODE"          NUMBER NOT NULL,
-    "DESCRIPTION"   VARCHAR2(4000) NOT NULL,
-    "CAUSE"         VARCHAR2(4000) NOT NULL,
-    "ACTION"        VARCHAR2(4000) NOT NULL,
-    constraint  "TS_ERROR_CODE_PK" primary key ("ERROR_CODE_ID")
-)
+CREATE TABLE  "TS_ERROR_CODE" 
+   (	"ERROR_CODE_ID" NUMBER NOT NULL ENABLE, 
+	"DESCRIPTION" VARCHAR2(4000) NOT NULL ENABLE, 
+	"CAUSE" VARCHAR2(4000) NOT NULL ENABLE, 
+	"ACTION" VARCHAR2(4000) NOT NULL ENABLE, 
+	"SOURCE_URL" VARCHAR2(1000) NOT NULL ENABLE, 
+	 CONSTRAINT "TS_ERROR_CODE_PK" PRIMARY KEY ("ERROR_CODE_ID") ENABLE
+   );
 /
-
-CREATE sequence "TS_ERROR_CODE_SEQ" 
-/
-
-CREATE trigger "BI_TS_ERROR_CODE"  
-  before insert on "TS_ERROR_CODE"              
-  for each row 
-begin  
-  if :NEW."ERROR_CODE_ID" is null then
-    select "TS_ERROR_CODE_SEQ".nextval into :NEW."ERROR_CODE_ID" from dual;
-  end if;
-end;
-/   
-
-alter table "TS_ERROR_CODE" add
-constraint "TS_ERROR_CODE_UK1" 
-unique ("CODE")
-/   
-
-alter table "TS_ERROR_CODE" add
-("SOURCE_URL" VARCHAR2(1000) NOT NULL)
-/   
