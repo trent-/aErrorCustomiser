67c6875cec355b68c8d90bbef830f97eb137452b
trent <trent.schafer@gmail.com>
2012-06-11 18:14:00 +1000

    Fetch error from ts_error_constraint table

--- ts_get_error.sql
+++ ts_get_error.sql
@@ -1,23 +1,35 @@
-create or replace function ts_get_error(
+create or replace
+function ts_get_error(
     p_error in apex_error.t_error )
     return apex_error.t_error_result
 is
     l_result          apex_error.t_error_result;
-    l_reference_id    number;
-    l_constraint_name varchar2(255);
+    l_constraint_name user_constraints.constraint_name%type;
+    l_constraint_count NUMBER;
 begin
     l_result := apex_error.init_error_result (
                     p_error => p_error );
- 
 
-    --TODO: logic to return a different error message
+
+      
+    l_constraint_name := apex_error.extract_constraint_name(p_error);
     
-    --fallback incase our testcase doesn't match anything
-    if p_error.ora_sqlcode is not null and l_result.message = p_error.message then --no new message yet assigned. Must mean we haven't met the conditions above
+    select count(*) into l_constraint_count
+    from ts_constraint_Error
+    where constraint_name = l_constraint_name;
+    
+    if l_constraint_count > 0 then
+      select constraint_name into l_result.message
+      from ts_constraint_error
+      where constraint_name = l_constraint_name;
+    else
+        --fallback incase our testcase doesn't match anything
+      if p_error.ora_sqlcode is not null and l_result.message = p_error.message then --no new message yet assigned. Must mean we haven't met the conditions above
         l_result.message := apex_error.get_first_ora_error_text(
-                p_error => p_error);
-    
+                  p_error => p_error);
+      end if;
     end if;
+
  
     return l_result;
 end ts_get_error;
