c8b9ab6f90403d29390d51c2ee829e85648f82c8
trent <trent.schafer@gmail.com>
2012-06-11 18:12:21 +1000

    Add table to manage errors for constraints

--- DDL.sql
+++ DDL.sql
@@ -44,4 +44,53 @@ end;
 
 /
 ALTER TRIGGER "BI_TS_REF_INTEGRIY_ORA" ENABLE;
-  
+/
+
+
+
+REM INSERTING into TS_REF_INTEGRITY
+SET DEFINE OFF;
+Insert into TS_REF_INTEGRITY (CONSTRAINT_TYPE,DESCRIPTION) values ('C','Check constraint on a table');
+Insert into TS_REF_INTEGRITY (CONSTRAINT_TYPE,DESCRIPTION) values ('P','Primary key');
+Insert into TS_REF_INTEGRITY (CONSTRAINT_TYPE,DESCRIPTION) values ('U','Unique key');
+Insert into TS_REF_INTEGRITY (CONSTRAINT_TYPE,DESCRIPTION) values ('R','Referential integrity');
+Insert into TS_REF_INTEGRITY (CONSTRAINT_TYPE,DESCRIPTION) values ('V','With check option, on a view');
+Insert into TS_REF_INTEGRITY (CONSTRAINT_TYPE,DESCRIPTION) values ('O','With read only, on a view');
+Insert into TS_REF_INTEGRITY (CONSTRAINT_TYPE,DESCRIPTION) values ('H','Hash expression');
+Insert into TS_REF_INTEGRITY (CONSTRAINT_TYPE,DESCRIPTION) values ('F','Constraint that involves a REF column');
+Insert into TS_REF_INTEGRITY (CONSTRAINT_TYPE,DESCRIPTION) values ('S','Supplemental logging');
+
+
+CREATE TABLE TS_CONSTRAINT_ERROR
+(
+  ID NUMBER NOT NULL 
+, CONSTRAINT_NAME VARCHAR2(30) NOT NULL 
+, ERROR_MESSAGE VARCHAR2(400) NOT NULL 
+, CONSTRAINT TS_CONSTRAINT_ERRORS_PK PRIMARY KEY 
+  (
+    ID 
+  )
+  ENABLE 
+);
+
+create sequence seq_TS_CONSTRAINT_ERROR;
+/
+
+
+  CREATE OR REPLACE TRIGGER "BI_TS_CONSTAINT_ERROR" 
+   before insert on "TS_CONSTRAINT_ERROR" 
+   for each row 
+begin  
+         select seq_TS_CONSTRAINT_ERROR.nextval into :NEW."ID" from dual; 
+end;
+
+/
+ALTER TRIGGER "BI_TS_CONSTAINT_ERROR" ENABLE;
+/
+
+ALTER TABLE TS_CONSTRAINT_ERROR
+ADD CONSTRAINT TS_CONSTRAINT_ERROR_UK1 UNIQUE 
+(
+  CONSTRAINT_NAME 
+)
+ENABLE;
