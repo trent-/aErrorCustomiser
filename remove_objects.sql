5a376e14fedc5ecc1c8f4ee6f4ad02735e7e9e7f
trent <trent.schafer@gmail.com>
2012-06-17 12:41:59 +1000

    Add deinstallation script

---
+++ remove_objects.sql
@@ -0,0 +1,7 @@
+drop table TS_CONSRAINT_TYPE purge;
+drop table ts_constraint_error purge;
+drop table ts_person purge;
+drop table ts_pet purge;
+drop function ts_get_error;
+drop function ts_get_search_condition;
+drop sequence SEQ_TS_CONSTRAINT_ERROR
